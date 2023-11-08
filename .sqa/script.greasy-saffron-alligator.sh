8310cb0db51408667d46d2f38216d5891f1dccab
orviz <orviz@ifca.unican.es>
2023-11-08 16:19:22 +0100

    Add JePL file structure

--- .sqa/script.greasy-saffron-alligator.sh
+++ .sqa/script.greasy-saffron-alligator.sh
@@ -4,5 +4,5 @@
 
 (
 cd github.com/interTwin-eu/itwinai &&
-    hadolint use-cases/mnist/torch/Dockerfile --failure-threshold error
+    hadolint use-cases/3dgan/Dockerfile use-cases/mnist/torch/Dockerfile --failure-threshold error
 )
