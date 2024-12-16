ef25417b4526c427ae6d6a094574d868424ffdb6
Pablo Orviz <orviz@ifca.unican.es>
2024-12-16 17:03:26 +0100

    Add JePL file structure

--- .sqa/script.ready-cerise-kangaroo.sh
+++ .sqa/script.ready-cerise-kangaroo.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/interTwin-eu/itwinai &&
-    pytest  --json-report  --json-report-indent 4
+    hadolint use-cases/3dgan/Dockerfile use-cases/mnist/torch/Dockerfile env-files/torch/Dockerfile env-files/tensorflow/Dockerfile env-files/torch/jupyter/Dockerfile --failure-threshold error
 )
