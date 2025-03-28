b73e134a7ccbd2a100ca8c9fc70f3b481878731b
Pablo Orviz <orviz@ifca.unican.es>
2025-03-28 08:19:32 +0100

    Add JePL file structure

--- .sqa/script.wimpy-periwinkle-opossum.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/interTwin-eu/itwinai &&
-    hadolint use-cases/3dgan/Dockerfile use-cases/mnist/torch/Dockerfile env-files/torch/Dockerfile env-files/tensorflow/Dockerfile env-files/torch/jupyter/Dockerfile tutorials/distributed-ml/torch-kubeflow-1/Dockerfile --failure-threshold error
-)
