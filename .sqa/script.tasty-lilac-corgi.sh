228a4309d7d3bf18a0ef73e2cc4d192acb527e1a
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-08-04 14:23:33 +0100

    Add JePL file structure

--- .sqa/script.tasty-lilac-corgi.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/interTwin-eu/itwinai &&
-    hadolint tutorials/distributed-ml/torch-kubeflow-1/Dockerfile env-files/tensorflow/Dockerfile use-cases/3dgan/Dockerfile use-cases/mnist/torch/Dockerfile --failure-threshold error
-)
