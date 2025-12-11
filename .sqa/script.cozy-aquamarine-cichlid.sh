730759859863a398b914b479eadcac0482d76aee
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-12-11 23:00:14 +0000

    Add JePL file structure

--- .sqa/script.cozy-aquamarine-cichlid.sh
+++ .sqa/script.cozy-aquamarine-cichlid.sh
@@ -5,5 +5,6 @@
 
 (
 cd github.com/interTwin-eu/itwinai &&
-    pytest  --json-report  --json-report-indent 4
+    make torch-env-cpu&&
+    .venv-pytorch/bin/pytest -v ./tests/ --disable-warnings -n logical --dist loadfile -m "not hpc and not memory_heavy and not tensorflow"
 )
